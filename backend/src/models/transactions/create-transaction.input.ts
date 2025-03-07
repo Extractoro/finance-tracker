216020b780120555ceee486c869226d8db8128de
Vadym <67538058+Extractoro@users.noreply.github.com>
2025-03-07 16:31:50 +0200

    fix: transaction creation

--- backend/src/models/transactions/create-transaction.input.ts
+++ backend/src/models/transactions/create-transaction.input.ts
@@ -1,4 +1,4 @@
-import { Field, Float, InputType } from '@nestjs/graphql';
+import { Field, Float, InputType, Int } from '@nestjs/graphql';
 import {
   IsDate,
   IsNotEmpty,
@@ -20,6 +20,10 @@ export class CreateTransactionInput {
   @Field(() => FinancialType)
   @IsNotEmpty({ message: 'Financial type must be filled' })
   type: FinancialType;
+
+  @Field(() => Int)
+  @IsNotEmpty({ message: 'Category must be filled' })
+  category_id: number;
 
   @Field(() => String, { nullable: true })
   @IsOptional()
--- backend/src/transactions/create-transaction/create-transaction.service.ts
+++ backend/src/transactions/create-transaction/create-transaction.service.ts
@@ -3,8 +3,8 @@ import { PrismaService } from '../../prisma.service';
 import { CreateTransactionInput } from '../../models/transactions/create-transaction.input';
 import { Args } from '@nestjs/graphql';
 import { CreateTransactionResponse } from '../../models/transactions/create-transaction.response';
-import { FinancialType } from '../../models/enums/financial-type.enum';
 import { ApolloError } from 'apollo-server-express';
+import { FinancialType } from '../../models/enums/financial-type.enum';
 
 @Injectable()
 export class CreateTransactionService {
@@ -13,26 +13,51 @@ export class CreateTransactionService {
   async createTransaction(
     @Args('data') args: CreateTransactionInput,
   ): Promise<CreateTransactionResponse> {
-    const createdTransaction = await this.prisma.transactions.create({
-      data: args,
-    });
+    try {
+      return await this.prisma.$transaction(async (prisma) => {
+        const existedUser = await prisma.users.findUnique({
+          where: { user_id: args.user_id },
+        });
 
-    if (!createdTransaction)
+        if (!existedUser)
+          throw new ApolloError('User not found', 'USER_NOT_FOUND');
+
+        const existedCategory = await prisma.category.findUnique({
+          where: { category_id: args.category_id },
+        });
+
+        if (!existedCategory)
+          throw new ApolloError('Category not found', 'CATEGORY_NOT_FOUND');
+
+        const createdTransaction = await prisma.transactions.create({
+          data: args,
+        });
+
+        if (!createdTransaction)
+          throw new ApolloError(
+            'Error creating transaction',
+            'ERROR_CREATING_TRANSACTION',
+          );
+
+        return {
+          success: true,
+          message: 'Transaction created',
+          errorCode: null,
+          transaction: {
+            ...createdTransaction,
+            type: createdTransaction.type as FinancialType,
+            amount: createdTransaction.amount as number,
+            date: createdTransaction.date ?? new Date(),
+          },
+        };
+      });
+    } catch (error) {
       throw new ApolloError(
-        'Error creating transaction',
-        'ERROR_CREATING_TRANSACTION',
+        error.message || 'Transaction creating failed',
+        error.extensions?.code || 'TRANSACTION_CREATING_FAILED',
       );
-
-    return {
-      success: true,
-      message: 'Transaction created',
-      errorCode: null,
-      transaction: {
-        ...createdTransaction,
-        type: createdTransaction.type as FinancialType,
-        amount: createdTransaction.amount as number,
-        date: createdTransaction.date ?? new Date(),
-      },
-    };
+    } finally {
+      await this.prisma.$disconnect();
+    }
   }
 }
