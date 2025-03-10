de093400bb232f33387b8a3056d79e48e3a24da5
Vadym <67538058+Extractoro@users.noreply.github.com>
2025-03-10 16:02:49 +0200

    fix: config cors

--- backend/src/configs/cors.config.ts
+++ backend/src/configs/cors.config.ts
@@ -1,5 +1,5 @@
 const corsConfig = {
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: [process.env.CLIENT_URL, process.env.CLIENT_DEV_URL],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   credentials: true,
