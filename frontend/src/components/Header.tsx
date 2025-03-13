20d42b64f6bd709b0250f3890340271ed3735bf4
Vadym <67538058+Extractoro@users.noreply.github.com>
2025-03-13 22:22:35 +0200

    style: container & header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import Link from 'next/link';
+import Container from '@/components/Container';
+import { RiDashboardLine } from 'react-icons/ri';
+import LogoIcon from '@/assets/logoIcon';
+import { GrTransaction } from 'react-icons/gr';
 
 const Header = () => {
   return (
-    <header>
-      <nav></nav>
-    </header>
+    <Container>
+      <header className="w-full max-w-screen-md py-3 px-6 mt-5 bg-header-background rounded-[2rem]">
+        <nav className='flex items-center justify-between gap-16'>
+          <Link href="/dashboard"><LogoIcon size={60} classname='fill-accent hover:fill-hover transition duration-300' /></Link>
+          <ul className='flex items-center justify-evenly flex-grow'>
+            <li>
+              <Link href="/dashboard"
+                    className="group flex items-center gap-2 text-link hover:text-hover transition duration-300">
+                <RiDashboardLine className="text-accent transition duration-300 group-hover:text-hover" size={24} />
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link href="/transactions"
+                    className="group flex items-center gap-2 text-link hover:text-hover transition duration-300">
+                <GrTransaction className="text-accent transition duration-300 group-hover:text-hover" size={24} />
+                Transactions
+              </Link>
+            </li>
+            <li>
+              <Link href="/transactions"
+                    className="group flex items-center gap-2 text-link hover:text-hover transition duration-300">
+                <GrTransaction className="text-accent transition duration-300 group-hover:text-hover" size={24} />
+                Transactions
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </Container>
   );
 };
 export default Header;
