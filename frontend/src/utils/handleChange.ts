93a7f18770b3e0a896a109c114f05635c52e1ed9
Vadym <67538058+Extractoro@users.noreply.github.com>
2025-03-14 19:00:05 +0200

    feat: transaction filter

--- frontend/src/utils/handleChange.ts
+++ frontend/src/utils/handleChange.ts
@@ -1,10 +1,29 @@
 import { ChangeEvent, Dispatch, SetStateAction } from 'react';
 
 const handleChange = <T>(
-  e: ChangeEvent<HTMLInputElement>,
+  e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   setFormData: Dispatch<SetStateAction<T>>,
+  setError?: Dispatch<SetStateAction<string | null>>
 ) => {
   const { name, value } = e.target;
+
+  if (name === "date" && setError) {
+    console.log(value);
+
+
+    const minDate = new Date("2020-01-01T00:00");
+    const maxDate = new Date();
+    const selectedDate = new Date(value);
+
+    if (selectedDate < minDate) {
+      setError("Date must be after January 1, 2020.");
+    } else if (selectedDate > maxDate) {
+      setError("Date cannot be in the future.");
+    } else {
+      setError(null);
+    }
+  }
+  
   setFormData(prevState => ({ ...prevState, [name]: value }));
 };
 
